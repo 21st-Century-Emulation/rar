cf84762f843763b95ab08453f4d2f2085b68b5a6
David Tyler <david.tyler@metaswitch.com>
2021-04-23 09:05:27 +0100

    Added healthcheck endpoint

--- main.go
+++ main.go
@@ -55,5 +55,8 @@ func rar(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/api/v1/execute", rar)
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Healthy")
+	})
 	http.ListenAndServe(":8080", nil)
 }
